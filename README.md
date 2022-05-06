307f4dc2619246780ed841da8af001de76740b4b
Ian Parks <ian.parks99@gmail.com>
2022-05-06 16:40:00 -0400

    main: added phaser

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# table_top
+# Table Top DnD
+Side project for my DnD group. This will be meant to be like a clone of astral table top.
+Since astral is no longer being actively worked on, I wanted to make an open-source version for my friends.
+
+Basic setup:
+* Vue 3
+* Phaser
+* MongoDB
+* NodeJS
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <board></board>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Board from "./components/Board.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Board
+  },
+  data() {
+    return {
+      
+    }
   }
 }
 </script>
@@ -21,6 +27,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
